0a4d93f90bd7098cea4624e5208d2c60ae676ade
khan <omeryagci3295@gmail.com>
2022-06-06 21:52:42 +0300

    bug fixed

--- app.js
+++ app.js
@@ -4,17 +4,47 @@ const todoList = document.querySelector(".todo-list");
 
 addToDoButton.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("zaa");
-  const eachToDoListEl = document.createElement("div");
-  eachToDoListEl.classList.add("todo");
-  eachToDoListEl.innerHTML = `
-  <li>${valueFromInput.value}</li>
-          <button class="btn-delete">
-            <i class="fa-solid fa-trash"></i>
-          </button>
-          <button class="btn-check">
-            <i class="fa-solid fa-check"></i>
-          </button>
-  `;
-  todoList.appendChild(eachToDoListEl);
+  // Add New To do Div
+
+  // Check for Value condition
+
+  if (!valueFromInput.value) {
+    return confirm("There is no ınput...");
+  } else {
+    const eachToDoListEl = document.createElement("div");
+    eachToDoListEl.classList.add("todo");
+
+    eachToDoListEl.innerHTML = `
+      <li>${valueFromInput.value}</li>
+              <button class="btn-delete">
+                <i class="fa-solid fa-trash"></i>
+              </button>
+              <button class="btn-check">
+                <i class="fa-solid fa-check"></i>
+              </button>
+      `;
+    valueFromInput.value = "";
+    todoList.appendChild(eachToDoListEl);
+  }
+
+  // Remove Button
+
+  const removeList = document.querySelectorAll(".btn-delete");
+  removeList.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.target.parentElement.classList.add("fall");
+      e.target.parentElement.addEventListener("transitionend", () => {
+        e.target.parentElement.remove();
+      });
+    });
+  });
+
+  // Check Button
+
+  const checkButton = document.querySelectorAll(".btn-check");
+  checkButton.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      e.target.parentElement.classList.toggle("checked");
+    });
+  });
 });
